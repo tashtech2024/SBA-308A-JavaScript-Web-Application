7536af5d5e364491c71d7b6d579ebff67bf6cbfd
Latasha Davis <latashaddavis21@gmail.com>
2024-05-09 22:57:21 -0400

    tried

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
 console.log('OOOHHHHH LETS DO IT!');
 console.log('Running.....');
-
-const button = document.getElementById('nav');
+const button = document.getElementById('sumbit');
 const products = document.getElementById('postProducts');
-const app = document.getElementById('app');
-const Http = new XMLHttpRequest();
-// const url='https://jsonplaceholder.typicode.com/posts';
-// Http.open("GET", url);
-// Http.send();
-// Http.onreadystatechange = (e) => {
-//   console.log(Http.responseText)
-// }
-// * Use the fetch API or Axios to communicate with an external web API. Use the data provided by this API to populate your application’s content and features ====//
 
+
+// * Use the fetch API or Axios to communicate with an external web API. Use the data provided by this API to populate your application’s content and features ====//
 fetch('https://dummyjson.com/products/')
 .then(res => res.json())
 .then(json => console.log(json))
             
-// button.addEventListener('sumbit', logsubmit);
+//? Product grab ===========================================//
 async function theShop() {
     try {
-        const response = await fetch("https://dummyjson.com/products/11");
+        const response = await fetch("https://dummyjson.com/products/1");
         console.log(response);
         const jsonData = await response.json()
         console.log(jsonData);
@@ -33,33 +25,30 @@ async function theShop() {
 }
 theShop();
 
-  
-  const input = document.getElementById("input");
-  
 
+//? Comment grab =======================================//
 async function grabComments() {
     try{
-    const response = await fetch(" https://dummyjson.com/comments/10");
-    console.log(response);
+    const response = await fetch(" https://dummyjson.com/comments/1");
     const jsonData = await response.json()
     console.log(jsonData);
     app.innerHTML = (`<h1>${jsonData.comments}</h1>
-   `);
-} catch (err) {
+    </h1> 
+    <h2>${jsonData.body}</h2>`);
+  } catch (err) {
     console.log(err);
+    console.log(response);
 
 }
 };
 grabComments();
 
-// button.addEventListener('click', theShop => {
-//     console.log('Button clicked!'); 
-// });
+
 //* Promise for user message for 50% off ==================================//
-const promise = new Promise((resolve, reject) => {
+const myFirstpromise = new Promise((resolve, reject) => {
   setTimeout(function() { myFunction(" TEXT NEWCUST TO 34344 AND GET 50% OFF!!!"); }, 7000);
 })
-console.log(promise);
+console.log(myFirstpromise);
 
 function myFunction(value) {
   document.getElementById("message").innerHTML = value;
